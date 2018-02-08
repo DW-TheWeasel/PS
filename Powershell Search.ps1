9ddd5b36274622bde832f221ae4a2d4513aee753
Danny Williams <Danny_Williams@symantec.com>
2018-02-08 10:34:08 -0800

    Added PS version

--- Powershell Search.ps1
+++ Powershell Search.ps1
@@ -1,10 +1,20 @@
-# Find all powershell processes and show command line used to launch
-# 
+<#
+Using this file for useful PS snippets
+
+To do:
+Check PS logging
+Make recommendations
+Check reg for mshta, ps entries
+Check reg for startup items
+Check reg for bogus extension handlers?
+#>
+
+Clear-Host # Start with blank host
+
 # Check PS version
-# Check PS logging
-# Make recommendations
-# 
-# Check reg for mshta, ps entries
-# Check reg for startup items
-# Check reg for bogus extension handlers?
+Write-Host ("You are running Powershell version " + $PSVersionTable.PSVersion.Major)
+Write-Host ($PSVersionTable | Out-String)
+
+
+# Find all powershell processes and show command line used to launch
 foreach($process in (Get-CimInstance Win32_Process -Filter "name = 'powershell.exe'")) {($process.Name + " (PID:" + $process.ProcessId + ")"); $process | Select-Object CommandLine | Format-Table -autosize -wrap}
